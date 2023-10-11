a6116431d489bbfe16952b3adefe3adb8a606042
nikita-krupenko <nikitoss.krupenko.github@gmail.com>
2023-10-11 18:01:55 +0300

    add new file

---
+++ testGit/vgntyngr.swift
@@ -0,0 +1,20 @@
+//
+//  vgntyngr.swift
+//  testGit
+//
+//  Created by nikita on 11.10.2023.
+//
+
+import SwiftUI
+
+struct vgntyngr: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct vgntyngr_Previews: PreviewProvider {
+    static var previews: some View {
+        vgntyngr()
+    }
+}
