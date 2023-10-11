b8dba2971f7f14e43a700316030cebee66f71995
nikita-krupenko <nikitoss.krupenko.github@gmail.com>
2023-10-11 11:45:18 +0300

    Add Name

--- testGit/ContentView.swift
+++ testGit/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, Nikita!")
                 .foregroundColor(Color.gray)
                 .background(Color(.green))
             
