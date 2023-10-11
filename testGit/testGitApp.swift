ba83227a056074de60dabe037fc50c831460293b
nikita-krupenko <nikitoss.krupenko.github@gmail.com>
2023-10-11 11:26:53 +0300

    Initial Commit

---
+++ testGit/testGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGitApp.swift
+//  testGit
+//
+//  Created by nikita on 11.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct testGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
